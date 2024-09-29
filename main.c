5bb656d6a614bcc08d38d8ba32f4fd480dbe4258
Ravi <trivediravi561@gmail.com>
2024-09-29 17:07:26 -0400

    update variables

--- main.c
+++ main.c
@@ -4,46 +4,47 @@ double dataset[10][2] = {{0, 2}, {1, 5}, {2, 6}, {3, 9}, {4, 11}, {5, 14}, {6, 1
 double output[10][2] = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}, {9, 0}};
 double weight = 0;
 double bias = 0;
-// Function to calculate Mean Squared Error
+int EPOCH = 100;
+int arrSize = sizeof(dataset) / sizeof(dataset[0]);
+
+// Calculate Error
 double MSE()
 {
   double res = 0;
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < arrSize; i++)
   {
     double temp = 0;
     temp = output[i][1] - dataset[i][1];
     res += pow(temp, 2);
   }
-  res = res / (float)10;
+  res = res / (float)arrSize;
   printf("LOSS: %f\n", res);
   return res;
 }
 
+// Adjust Weights
 void gradientDescent()
 {
   double change = 0.01;
   double weightRes = 0;
   double biasRes = 0;
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < arrSize; i++)
   {
     double weightTemp = 0;
     double biasTemp = 0;
     weightRes += (output[i][1] - dataset[i][1]) * 2 * i;
     biasRes += (output[i][1] - dataset[i][1]) * 2;
   }
-  weightRes = weightRes / (float)10;
-  biasRes = biasRes / (float)10;
-  printf("WEIGHT RES: %f\n", weightRes);
-  printf("BIAS RES: %f\n", biasRes);
+  weightRes = weightRes / (float)arrSize;
+  biasRes = biasRes / (float)arrSize;
   weight = weight - (change * weightRes);
   bias = bias - (change * biasRes);
-  printf("WEIGHT: %f\n", weight);
-  printf("BIAS: %f\n", bias);
 }
 
+//Update Output with new weights and bias
 void calcOutput()
 {
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < arrSize; i++)
   {
     output[i][1] = weight * i + bias;
   }
@@ -51,7 +52,9 @@ void calcOutput()
 
 int main()
 {
-  for (int i = 0; i < 10; i++)
+
+  //Loop based on epoch
+  for (int i = 0; i < EPOCH; i++)
   {
     MSE();
     gradientDescent();
